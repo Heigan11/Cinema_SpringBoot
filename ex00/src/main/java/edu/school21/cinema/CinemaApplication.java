7c9b965d0d3a369d59f651e723df117eca9081a3
Jraye <heigan11@gmail.com>
2023-01-09 16:50:07 +0300

    before security

--- ex00/src/main/java/edu/school21/cinema/CinemaApplication.java
+++ ex00/src/main/java/edu/school21/cinema/CinemaApplication.java
@@ -11,5 +11,4 @@ public class CinemaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CinemaApplication.class, args);
     }
-
 }
--- ex00/src/main/java/edu/school21/cinema/config/SecurityConfig.java
+++ ex00/src/main/java/edu/school21/cinema/config/SecurityConfig.java
@@ -1,38 +1,38 @@
 package edu.school21.cinema.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-@Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    private final PasswordEncoder passwordEncoder;
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-    }
-
-    @Bean
-    @Override
-    protected UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("admin")
-                        .password(passwordEncoder.encode("111"))
-                        .roles("ADMIN")
-                        .build()
-        );
-    }
-}
+//@Configuration
+//@EnableWebSecurity
+//@RequiredArgsConstructor
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    private final PasswordEncoder passwordEncoder;
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        super.configure(http);
+//    }
+//
+//    @Bean
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        return new InMemoryUserDetailsManager(
+//                User.builder()
+//                        .username("admin")
+//                        .password(passwordEncoder.encode("111"))
+//                        .roles("ADMIN")
+//                        .build()
+//        );
+//    }
+//}
